19d58b096c739722af4bba0a972a6ed014417967
Yariv Fishman <yarivfishman0@gmail.com>
2022-11-12 16:30:24 +0200

    Add app base

---
+++ main.py
@@ -0,0 +1,10 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='127.0.0.1', port=8005, log_level="info", reload=True)
+    print("running")
